4a69196f387620a80d94c03a786fc78a8e9de59c
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-04 18:33:26 +0100

    start with JsFundamentals

---
+++ JsFundamentals.js
@@ -0,0 +1,10 @@
+
+let numbers = [1 , 2 , 3 , 4 , 5];
+
+for(i=1 ; i <= numbers.length ; i++){
+    if(i % 2 == 0 ){
+        console.log(i + " is even")
+    }else {
+        console.log(i + " is odd")
+    }
+}
