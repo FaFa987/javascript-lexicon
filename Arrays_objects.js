2a74e6a9a6b5aa8b1bff73e022e0d838b79f0ff4
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-07 01:00:43 +0100

    .forEach

--- Arrays_objects.js
+++ Arrays_objects.js
@@ -9,12 +9,16 @@ numbers.push(5, 6, 7);
 
 numbers.pop();
 // console.log(numbers); // [1, 2, 3, 4, 8, 5, 6]
+numbers = [1, 2, 3, 4];
 
 const filteredArray = numbers.filter (n => n % 2 == 0);
 console.log(filteredArray);
 
 const squaredNumbers = numbers.map((n) => n * n);
 console.log(squaredNumbers);
+
+numbers.forEach( num => console.log(num));
+
 
 
 
