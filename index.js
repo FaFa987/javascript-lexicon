52db44aa8dfd0b05dfe53f21151fab712162063f
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-16 17:19:32 +0100

    table / table.id / table.classList

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("javascript")
+console.log("javascript :D");
+
+//Might be a result of a Get-Request
+const data = [
+    {firstname: "John", lastname: "Doe", email: "john@example.com"},
+    {firstname: "Mary", lastname: "Moe", email: "mary@example.com"},
+    {firstname: "July", lastname: "Dooley", email: "july@example.com"},
+]
+
+const table = document.createElement("table"); // <table></table>
+table.id = "dynamicTable"; // <table id="dynamicTable"></table>
+table.classList.add("table" , "table-striped") // <table id="dynamicTable" class="table table-striped">
