07bcd5473db1d7ce9b4c3553b591639ab0c3e157
Fatemeh <f.khalleghi.777@gmail.com>
2025-03-16 22:04:25 +0100

    if() {} --> just for one Time

--- index.js
+++ index.js
@@ -8,52 +8,57 @@ const data = [
 ]
 
 const showData = () => {
-
-    const table = document.createElement("table"); // <table></table>
-table.id = "dynamicTable"; // <table id="dynamicTable"></table>
-table.classList.add("table" , "table-striped") // <table id="dynamicTable" class="table table-striped">
-
-const thead = document.createElement("thead");
-const headerRow = document.createElement("tr");
-
-const headerText = ["Firstname","Lastname","Email"];
-
-headerText.forEach(headertext => {
-    const th = document.createElement("th");
-    th.textContent = headertext;
-
-    headerRow.appendChild(th);
-});
-
-thead.appendChild(headerRow);
-table.appendChild(thead);
-
-const dataTableSection = document.getElementById("data-table");
-dataTableSection.appendChild(table);
-
-
-const tbody = document.createElement("tbody");
-
-    data.forEach( person => {
-      const tr = document.createElement("tr");
-
-      const td1 = document.createElement("td");
-      td1.textContent = person.firstname;
-      tr.appendChild(td1);
-
-      const td2 = document.createElement("td");
-      td2.textContent = person.lastname;
-      tr.appendChild(td2);
-
-      const td3 = document.createElement("td");
-      td3.textContent = person.email;
-      tr.appendChild(td3);
-
-      tbody.appendChild(tr);
-    });
-
-    table.appendChild(tbody);
     
+    if(!document.getElementById("dynamicTable")){
+        const table = document.createElement("table"); // <table></table>
+        table.id = "dynamicTable"; // <table id="dynamicTable"></table>
+        table.classList.add("table" , "table-striped") // <table id="dynamicTable" class="table table-striped">
+        
+        const thead = document.createElement("thead");
+        const headerRow = document.createElement("tr");
+        
+        const headerText = ["Firstname","Lastname","Email"];
+        
+        headerText.forEach(headertext => {
+            const th = document.createElement("th");
+            th.textContent = headertext;
+        
+            headerRow.appendChild(th);
+        });
+        
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+        
+        const dataTableSection = document.getElementById("data-table");
+        dataTableSection.appendChild(table);
+        
+        
+        const tbody = document.createElement("tbody");
+        
+            data.forEach( person => {
+              const tr = document.createElement("tr");
+        
+              const td1 = document.createElement("td");
+              td1.textContent = person.firstname;
+              tr.appendChild(td1);
+        
+              const td2 = document.createElement("td");
+              td2.textContent = person.lastname;
+              tr.appendChild(td2);
+        
+              const td3 = document.createElement("td");
+              td3.textContent = person.email;
+              tr.appendChild(td3);
+        
+              tbody.appendChild(tr);
+            });
+        
+            table.appendChild(tbody);
+    }else {
+        console.log("Table is already present!")
+    }
+
+
 }
 
     /*
